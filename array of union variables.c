cdced67503bddfeca17f2440b2b0af4db3a96e2b
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-08-31 21:51:08 +0530

    Create array of union variables.c

---
+++ array of union variables.c
@@ -0,0 +1,24 @@
+// Arrays of union variables
+
+#include<stdio.h>
+union POINT
+{
+    int x, y;
+}POINT;
+int main()
+{
+    int i;
+    union POINT points[3];
+    points[0].x = 2;
+    points[0].y = 3;
+    points[1].x = 4;
+    points[1].y = 5;
+    points[2].x = 6;
+    points[2].y = 7;
+
+    for(i=0;i<3;i++)
+    {
+        printf("\n Co-ordinates of point[%d] are %d and %d",i,points[i].x , points[i].y);
+    }
+    return 0;
+}
